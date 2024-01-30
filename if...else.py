d040eb3a52cf8263ec56b5b2c80d21be749a803b
zhaqsylyq <60311066+zhaqsylyqq@users.noreply.github.com>
2024-01-30 16:58:21 +0600

    lab2

---
+++ if...else.py
@@ -0,0 +1,53 @@
+# Print "Hello World" if a is greater than b.
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+# Print "Hello World" if a is not equal to b.
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+# Print "Yes" if a is equal to b, otherwise print "No".
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+# Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3"
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+# Print "Hello" if a is equal to b, and c is equal to d.
+if a == b and c == d:
+    print("Hello")
+
+# Print "Hello" if a is equal to b, or if c is equal to d.
+if a == b or c == d:
+    print("Hello")
+
+# Complete the code block, print "YES" if 5 is larger than 2
+if 5 > 2:
+    print("YES")
+
+# Use the correct one line short hand syntax to print "YES" if a is equal to b, otherwise print("NO").
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+
+# Use an if statement to print "YES" if either a or b is equal to c.
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES")
---
+++ tuples.py
@@ -0,0 +1,15 @@
+# Use the correct syntax to print the first item in the fruits tuple.
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+# Use the correct syntax to print the number of items in the fruits tuple.
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+# Use negative indexing to print the last item in the tuple.
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+# Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
